079de7717cbe97c78a95e9330891b6435c01b61f
Lim Qun Da <qunda@Lims-MacBook-Air.local>
2021-03-05 14:30:58 +0800

    refactor App compoennt to show UserPage

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import transaction from "./pages/transaction";
 import PricePage from "./pages/PricePage";
-import UserShow from "./components/UserShow";
+import User from "./pages/User";
 
-function myUserShow() {
-  const user = {
-    phoneNumber: "(+65) 90721077",
-    accountKey: "554fd8be-b1dd-4430-b150-4d3335ef8427",
-    lastName: "Stracke",
-    username: "Group2",
-    address: "4337 Vernon Lodge",
-    email: "group2@techtrek.com",
-    firstName: "Leopoldo",
-    nric: "S63336905T",
-  };
-  return <UserShow user={user} />;
-}
+// function myUserShow() {
+//   const user = {
+//     phoneNumber: "(+65) 90721077",
+//     accountKey: "554fd8be-b1dd-4430-b150-4d3335ef8427",
+//     lastName: "Stracke",
+//     username: "Group2",
+//     address: "4337 Vernon Lodge",
+//     email: "group2@techtrek.com",
+//     firstName: "Leopoldo",
+//     nric: "S63336905T",
+//   };
+//   return <UserShow user={user} />;
+// }
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
       <Router>
         <Route exact path="/price" component={PricePage} />
         <Route exact path="/transaction" component={transaction} />
-        <Route exact path="/user" component={myUserShow} />
+        <Route exact path="/user" component={User} />
       </Router>
     </div>
   );
