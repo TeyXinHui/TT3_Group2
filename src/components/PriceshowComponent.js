f2921759b604633c0ced70ed0ee7d94632fa74b6
Li Ying Kwa <li5ying@hotmail.com>
2021-03-05 13:08:53 +0800

    done with priceshow

--- src/components/PriceshowComponent.js
+++ src/components/PriceshowComponent.js
@@ -1,22 +1,80 @@
 import React,{Component} from 'react';
+import "./PriceshowComponent.css"
 
 class Priceshow extends Component {
 
     constructor(){
         super();
-        this.state = {}
+        this.state = {
+            data: [
+                {price: "NIL", assetSymbol: "NIL", timestamp: "NIL"}
+            ]
+        }
     }
 
-    componentDidMount(){
+/*     componentDidMount(){
 
         console.log("Component has mounted")
+        this.fetchData()
 
+    } */
+
+    async fetchData() {
+        try{
+
+            // Fetch data from api given
+            const response = await fetch('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current', {
+                method: 'POST',
+                headers: {
+                    'x-api-key' : 'PXeFPCR73iU4khaSqcRM1KBX5SblxPm4R1Rzt07a'
+                }
+            })
+            .then(response => response.json())
+            .then(newdata => this.setState({
+                data: [newdata]
+            }));
+
+        }
+
+        catch(err) {
+            console.log(err);
+        }
     }
 
-    render(){
+
+    renderTableHeader() {
+        let header = Object.keys(this.state.data[0])
+        console.log(header)
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+     }
+  
+
+    renderTableData() {
+        return this.state.data.map((entry, index) => {
+            const { price, assetSymbol, timestamp } = entry
+            return (
+                <tr key={price}>
+                    <td>{price}</td>
+                    <td>{assetSymbol}</td>
+                    <td>{timestamp}</td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
         return (
-            <div classname = "container">
-                <h1>test test</h1>
+            <div className = "container">
+                <h1 id='title'>Show Prices</h1>
+                <button onClick={this.fetchData.bind(this)}>Refresh</button>
+                <table id='showpricetable'>
+                    <tbody>
+                        <tr>{this.renderTableHeader()}</tr>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
             </div>
         );
     }
